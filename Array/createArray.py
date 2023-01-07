dbdd4d22b694fa2b40bdf635c0ab46fa4f5cee0b
kinkeadian25 <kinkeadian@gmail.com>
2023-01-06 23:41:05 -0800

    boom

---
+++ Array/createArray.py
@@ -0,0 +1,23 @@
+from array import *
+
+#1. create an array and traverse
+arr1 = array('i', [1,2,3,4,5])
+
+for i in arr1:
+    print(i)
+#2. access individual elements through indexes
+
+#3. append any value to the array using append() method
+#4. insert value in an array using insert() method
+#5. extend python array using extend() method
+#6. add items from list into array using fromlist() method
+#7. remove any array element using remove() method
+#8. remove last array element using pop() method
+#9. fetch any element through its index using index() method
+#10. reverse a python array using reverse() method
+#11. get array buffer information through buffer_info() method
+#12. check for number of occurrences of an element using count() method
+#13. convert array into string using tostring() method
+#14. convert array to a python list with same elements using tolist() method
+#15. append a string to char array using fromstring() method
+#16. slice elements from an array
