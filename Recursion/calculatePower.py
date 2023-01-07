c357c1df31ff43e22eb69e78d4a642fae1c84117
= <=>
2023-01-06 19:34:47 -0800

    new stuff

--- Recursion/calculatePower.py
+++ Recursion/calculatePower.py
@@ -3,8 +3,9 @@ def calculatePower(base, exp):
     if exp == 0:
         return 1
     elif exp < 0:
+        assert base != 0, 'Zero Division not allowed'
         return 1/base * calculatePower(base, exp+1)
     else:
         return base * calculatePower(base, exp-1)
 
-print(calculatePower(4, 2))
+print(calculatePower(0, 2))
--- Recursion/recursionFactorial.py
+++ Recursion/recursionFactorial.py
@@ -1,8 +1,10 @@
 def factorial(n):
-    assert n >=0 and int(n) == n, 'The number must be positive integer'
+    # assert n >=0 and int(n) == n, 'The number must be positive integer'
     if n == 0:
         return 1
+    elif n < 0:
+        return n * factorial(n+1)
     else:
         return n * factorial(n-1)
 
-print(factorial(10))
+print(factorial(-5))
