a077cff44a18a79c617bebe131f2c7d5b8ccd8f1
= <=>
2023-01-08 19:20:03 -0800

    dict functions

---
+++ Dictionaries/dictMethods.py
@@ -0,0 +1,29 @@
+myDict = {'name': 'Ian', 'age': 26, 'address': 'Seattle', 'education': 'associates'}
+
+# myDict.clear()
+# print(myDict)
+
+# newDict = myDict.copy()
+# print(newDict)
+
+# newDict = {}.fromkeys([1,2,3], 0)
+# print(newDict)
+
+# print(myDict.get('age', 27))
+
+# print(myDict.items())
+
+# print(myDict.keys())
+
+# print(myDict.popitem())
+
+# print(myDict.setdefault('name1', 'added'))
+# print(myDict)
+
+# print(myDict.pop('age'))
+
+# print(myDict.values())
+
+newDict = {'a':1, 'b':2, 'c':3}
+myDict.update(newDict)
+print(myDict)
